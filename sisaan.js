97b1564f5e0ba3283f9552368b830da2b91f5173
Jussin-koodi <85584367+Jussin-koodi@users.noreply.github.com>
2021-07-20 19:38:29 +0300

    Add files via upload
    uudet credentiaalit

--- sisaan.js
+++ sisaan.js
@@ -8,14 +8,14 @@ function authenticate() {
             function(err) { console.error("Error signing in", err); });
 }
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyDlfsLLLheFOvTeOtq0wFGWudkiJxVqRCQ");
+  gapi.client.setApiKey("AIzaSyBd_ELyTOGDB3sYWs0PM_a9qI2YycaAMQg");
   return gapi.client.load("https://photoslibrary.googleapis.com/$discovery/rest?version=v1")
       .then(function() { console.log("GAPI client loaded for API"); },
             function(err) { console.error("Error loading GAPI client for API", err); });
 }
 
 gapi.load("client:auth2", function() {
-  gapi.auth2.init({client_id: "306709585419-dgkqhgn9k9ut0ievoj46k545bumavgu6.apps.googleusercontent.com",
+  gapi.auth2.init({client_id: "615580118109-fudch3jka7pe9sechhn2nm3bt6l32lfp.apps.googleusercontent.com",
   immediate: true,
   fetch_basic_profile: true
 });
