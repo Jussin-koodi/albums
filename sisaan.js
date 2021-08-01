b5b998858fd83b7b85c23a14b359704ec6aaa1df
Jussin-koodi <85584367+Jussin-koodi@users.noreply.github.com>
2021-08-01 23:29:58 +0300

    Update sisaan.js

--- sisaan.js
+++ sisaan.js
@@ -2,20 +2,19 @@
 
 function authenticate() {
   return gapi.auth2.getAuthInstance()
-      .signIn({scope: "https://www.googleapis.com/auth/photoslibrary.sharing"})
-      //"https://www.googleapis.com/auth/photoslibrary.sharing"})
+      .signIn({scope: "https://www.googleapis.com/auth/photoslibrary.readonly"})
       .then(function() { console.log("Sign-in successful"); },
             function(err) { console.error("Error signing in", err); });
 }
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyDlfsLLLheFOvTeOtq0wFGWudkiJxVqRCQ");
+  gapi.client.setApiKey("YOUR API KEY");
   return gapi.client.load("https://photoslibrary.googleapis.com/$discovery/rest?version=v1")
       .then(function() { console.log("GAPI client loaded for API"); },
             function(err) { console.error("Error loading GAPI client for API", err); });
 }
 
 gapi.load("client:auth2", function() {
-  gapi.auth2.init({client_id: "306709585419-dgkqhgn9k9ut0ievoj46k545bumavgu6.apps.googleusercontent.com",
+  gapi.auth2.init({client_id: "YOUR CLIENT ID",
   immediate: true,
   fetch_basic_profile: true
 });
